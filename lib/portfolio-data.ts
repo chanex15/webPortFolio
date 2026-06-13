fa75bad3695c534626c9700c502a081fe86714da
chanex15 <167178682+chanex15@users.noreply.github.com>
2026-06-13 18:03:56 +0800

    Update portfolio-data.ts

--- lib/portfolio-data.ts
+++ lib/portfolio-data.ts
@@ -31,7 +31,7 @@ export const aboutParagraphs = [
 export const stats = [
   { num: '22', label: 'Years Old' },
   { num: '2025-2026', label: 'IT Graduate' },
-  { num: 'CDO', label: 'Philippines' },
+  { num: 'Cagayan De Oro City', label: 'Philippines' },
 ]
 
 export type Project = {
